4814c9ec9c5dcbccd7b244b407244d70ddff5c7b
choi-jayoon <aaa4165@naver.com>
2023-11-24 16:43:34 +0900

    로그아웃

--- src/components/Header.js
+++ src/components/Header.js
@@ -194,9 +194,24 @@ const Menu=styled.div`
    
 `;
 const onLogout=()=>{
-  localStorage.clear()
-  window.location.replace("/")
-  console.log('로그아웃 했습니다.')
+  const token = window.localStorage.getItem('token');
+    // console.log('access token',token)
+  fetch('http://ec2-13-124-237-120.ap-northeast-2.compute.amazonaws.com:8000/logout/',{
+    method:'POST',
+    hearders:{
+      'Content-Type':'application/json; charset=utf-8'
+    },
+    body:JSON.stringify({
+      access_token:token
+    }),
+  })
+  .then(res=>res.json())
+  .then(res=>{
+    localStorage.clear()
+    window.location.replace("/")
+    console.log('로그아웃 했습니다.')
+  })
+
 }
 export default function Header(props) {
   return (
