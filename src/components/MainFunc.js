177164eb92808a740d8f2b8833b5c0229660fdba
choi-jayoon <aaa4165@naver.com>
2023-11-27 23:52:13 +0900

    메인-비디오 추가

--- src/components/MainFunc.js
+++ src/components/MainFunc.js
@@ -41,15 +41,13 @@ const VideoRecorder = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     // state에 저장한 값을 가져옵니다.
-    const formData = new FormData();
-    formData.append('text',text)
-    fetch('http://ec2-13-124-237-120.ap-northeast-2.compute.amazonaws.com:8000/main/recommendation',{
+    fetch('http://ec2-13-124-237-120.ap-northeast-2.compute.amazonaws.com:8000/main/recommendation/',{
       method:'POST',
       hearders:{
         'Content-Type':'application/json; charset=utf-8'
       },
       body:JSON.stringify({
-        text:formData
+        text:text
       }),
     })
     .then(res=>res.json())
@@ -60,84 +58,88 @@ const VideoRecorder = () => {
     })
   };
 
-  // const { onCreate } = useContext(DiaryDispatchContext);
-
-	// const handleSubmit = () => {
-  //       if (content.length < 1) {
-  //           contentRef.current.focus();
-  //           return;
-  //       }
-
-  //       onCreate(data, content);
-  //       navigate("/", { replace: true });
-  //   }
 
 //   //비디오 전달
-//   const getMediaPermission = useCallback(async () => {
-//     try {
-//       const audioConstraints = { audio: true };
-//       const videoConstraints = {
-//         audio: false,
-//         video: true,
-//       };
+  const getMediaPermission = useCallback(async () => {
+    try {
+      const audioConstraints = { audio: true };
+      const videoConstraints = {
+        audio: false,
+        video: true,
+      };
 
-//       const audioStream = await navigator.mediaDevices.getUserMedia(
-//           audioConstraints
-//       );
-//       const videoStream = await navigator.mediaDevices.getUserMedia(
-//           videoConstraints
-//       );
+      const audioStream = await navigator.mediaDevices.getUserMedia(
+          audioConstraints
+      );
+      const videoStream = await navigator.mediaDevices.getUserMedia(
+          videoConstraints
+      );
 
-//       if (videoRef.current) {
-//           videoRef.current.srcObject = videoStream;
-//       }
+      if (videoRef.current) {
+          videoRef.current.srcObject = videoStream;
+      }
 
-//       // MediaRecorder 추가
-//       const combinedStream = new MediaStream([
-//           ...videoStream.getVideoTracks(),
-//         ...audioStream.getAudioTracks(),
-//       ]);
+      // MediaRecorder 추가
+      const combinedStream = new MediaStream([
+          ...videoStream.getVideoTracks(),
+        ...audioStream.getAudioTracks(),
+      ]);
 
-//       const recorder = new MediaRecorder(combinedStream, {
-//           mimeType: 'video/webm',
-//       });
+      const recorder = new MediaRecorder(combinedStream, {
+          mimeType: 'video/webm',
+      });
 
-//       recorder.ondataavailable = (e) => {
-//           if (typeof e.data === 'undefined') return;
-//         if (e.data.size === 0) return;
-//         videoChunks.current.push(e.data);
-//       };
+      recorder.ondataavailable = (e) => {
+          if (typeof e.data === 'undefined') return;
+        if (e.data.size === 0) return;
+        videoChunks.current.push(e.data);
+      };
 
-//       mediaRecorder.current = recorder;
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }, []);
+      mediaRecorder.current = recorder;
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-//   const downloadVideo = () => {
-//     const videoBlob = new Blob(videoChunks.current, { type: mimeType });
-//     const videoUrl = URL.createObjectURL(videoBlob);
-//     const link = document.createElement('a');
-//     link.download = `My video - ${dayjs().format('YYYYMMDD')}.webm`;
-//     link.href = videoUrl;
-//     document.body.appendChild(link);
-//     link.click();
-//     document.body.removeChild(link);
-//   };
+  const downloadVideo = () => {
+    const videoBlob = new Blob(videoChunks.current, { type: mimeType });
+    const videoUrl = URL.createObjectURL(videoBlob);
+    const link = document.createElement('a');
+    link.download = `My video - ${dayjs().format('YYYYMMDD')}.webm`;
+    link.href = videoUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
 
-//   const sendVideo=()=>{
-//     const formData = new FormData();
-//     formData.append("video", mediaRecorder.current); 
-//     axios.post('http://ec2-13-124-237-120.ap-northeast-2.compute.amazonaws.com:8000/main/recommendation/', formData, { // 요청
-//     headers: {
-//       'Content-Type': 'multipart/form-data'
-//     }
-// })
-//   }
-//   useEffect(() => {
-//     getMediaPermission();
-//   }, []);
+  const sendVideo=()=>{
+    const formData = new FormData();
+    formData.append("video", mediaRecorder.current); 
+    fetch('http://ec2-13-124-237-120.ap-northeast-2.compute.amazonaws.com:8000/main/recommendation/',{
+      method:'POST',
+      hearders:{
+        'Content-Type': 'multipart/form-data'
+      },
+      body:{
+        formData
+      },
+    })
+    .then(res=>res.json())
+    .then(res=>{
+      console.log(res)
+      console.log('성공')
+      // window.location.replace('/question')
+    })
+    // axios.post('http://ec2-13-124-237-120.ap-northeast-2.compute.amazonaws.com:8000/main/recommendation/', formData, { // 요청
+    // headers: {
+    //   'Content-Type': 'multipart/form-data'
+    // }
+    //})
+  }
+  useEffect(() => {
+    getMediaPermission();
+  }, []);
 
   return (
     
@@ -161,11 +163,12 @@ const VideoRecorder = () => {
         </div>
         <div className='submitdiv'>
           <button class="submit" type='submit' onClick={submitHandler}>Submit</button>
+          <button class="submit" type='submit' onClick={sendVideo}>Video Submit</button>
         </div>
       </form>
-      {/* <button onClick={() => mediaRecorder.current?.start()}>Start Recording</button>
+      <button onClick={() => mediaRecorder.current?.start()}>Start Recording</button>
       <button onClick={() => mediaRecorder.current?.stop()}>Stop Recording</button>
-      <button onClick={downloadVideo}>Download</button> */}
+      <button onClick={downloadVideo}>Download</button>
 
     </AllWrapper>
   );
